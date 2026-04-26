4affa013fe33c14aa987cac3e16464941ed97ec7
Md Akhtar <104804456+mdakhtar03@users.noreply.github.com>
2026-04-27 03:34:42 +0530

    Time: 0 ms (100%), Space: 9.8 MB (48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-
-        if(s==t){
-            return 1;
-        }
-        else{
-            return 0;
-        }
+       vector<int> alphas(26,0);
+       if(s.size()!=t.size()){
+        return false;
+       }
+       for(int i=0;i<s.size();i++){
+            alphas[s[i]-'a']++;
+            alphas[t[i]-'a']--;
+       }
+       for(int i=0;i<alphas.size();i++){
+            if(alphas[i] != 0){
+                return false;
+            }
+       }
+       return true;
     }
 };
