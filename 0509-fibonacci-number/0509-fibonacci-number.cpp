74c48fc0468f69d20fb2218e9a52495ad3492620
Md Akhtar <104804456+mdakhtar03@users.noreply.github.com>
2026-06-17 06:33:53 +0530

    Time: 3 ms (64.19%), Space: 8.3 MB (11.32%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
-    int fib(int n) {
-        if(n==1){
-            return 1;
-        }
-        if(n==0){
-            return 0;
+    int solveUsingRec(int n){
+        if(n==0 || n==1){
+            return n;
         }
 
-        int recAns = fib(n-1) + fib(n-2);
+        int recAns = solveUsingRec(n-1) + solveUsingRec(n-2);
         return recAns;
+    }
+    int solveUsingMem(int n, vector<int> &dp){
+        if(n==0 || n==1){
+            return n;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        int ans = solveUsingMem(n-1,dp) + solveUsingMem(n-2,dp);
+        dp[n] = ans;
+        return dp[n];
+    }
+
+
+    int fib(int n) {
+        vector<int> dp(n+1,-1);
+        return solveUsingMem(n,dp);
     }
 };
