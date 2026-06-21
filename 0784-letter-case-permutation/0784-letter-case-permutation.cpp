9a427fff55e7ad182a9965ede6e1f20d8f6407b0
Md Akhtar <104804456+mdakhtar03@users.noreply.github.com>
2026-06-21 16:53:55 +0530

    Time: 6 ms (18.65%), Space: 14.2 MB (63.38%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void solveUsingRec(string s, vector<string> &ans, int index,
+    string store){
+        //Base Case
+        if(index == s.size()){
+            ans.push_back(store);
+            return;
+        }
+        //Include
+        char ch = s[index];
+        if(s[index]>='a' && s[index]<='z'){
+            ch = s[index] - 'a' + 'A';
+        }
+        else if(s[index]>='A' && s[index]<='Z'){
+            ch = s[index] - 'A' + 'a';
+        }
+        store.push_back(ch);
+        solveUsingRec(s,ans,index+1,store);
+
+        //exlcude
+        store.pop_back();
+        store.push_back(s[index]);
+        if(isalpha(ch))
+        solveUsingRec(s,ans,index+1,store);
+        
+    }
+    vector<string> letterCasePermutation(string s) {
+        vector<string> ans;
+        string store;
+        solveUsingRec(s,ans,0,store);
+        return ans;
+    }
+};
