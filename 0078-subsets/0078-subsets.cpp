a5013260b0658b3e14d52fb44bf73ee0e8f208d9
Md Akhtar <104804456+mdakhtar03@users.noreply.github.com>
2026-06-21 15:41:40 +0530

    Time: 0 ms (100%), Space: 9.9 MB (81.4%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-    vector<int> solveUsingRec(vector<int>& nums, vector<vector<int>> &ans,vector<int> &store,int index){
-        //Base case
-        vector<int> include;vector<int> exclude;
-        if(index == nums.size()){
-            //store ans
+    void solveUsingRec(vector<int>& nums,
+                       vector<vector<int>>& ans,
+                       vector<int>& store,
+                       int index) {
+
+        if(index == nums.size()) {
             ans.push_back(store);
-            return store;
+            return;
         }
+
+        // Include
         store.push_back(nums[index]);
-        include = solveUsingRec(nums, ans,store, index+1);
+        solveUsingRec(nums, ans, store, index + 1);
+
+        // Backtrack
         store.pop_back();
-        exclude = solveUsingRec(nums, ans,store, index+1);
-        return store;
+
+        // Exclude
+        solveUsingRec(nums, ans, store, index + 1);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
