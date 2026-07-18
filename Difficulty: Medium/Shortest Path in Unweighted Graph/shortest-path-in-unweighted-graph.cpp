82c1f78cb429ec3b6161b5221f685b0c6f4bdc09
Md Akhtar <104804456+mdakhtar03@users.noreply.github.com>
2026-07-18 14:04:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Path in Unweighted Graph/shortest-path-in-unweighted-graph.cpp
+++ Difficulty: Medium/Shortest Path in Unweighted Graph/shortest-path-in-unweighted-graph.cpp
@@ -1,22 +1,27 @@
 class Solution {
   public:
     int shortestPath(int V, vector<vector<int>> &edges, int src, int dest) {
-        //AdjcencyList
-        unordered_map<int,vector<int>> adjList;
+        //Adjcency List
+        unordered_map<int, vector<int>> adjList;
         for(auto edge:edges){
             int u = edge[0];
             int v = edge[1];
             adjList[u].push_back(v);
             adjList[v].push_back(u);
         }
-        //Path List
-        unordered_map<int,int> path;
-        queue<int> q;
+        //Parent List
+        unordered_map<int,int> parent;
+        //Visited true
         unordered_map<int,bool> visited;
+        //Queue for bfs
+        queue<int> q;
         
+        //Mark for source Node
         q.push(src);
-        path[src] = -1;
+        parent[src] = -1;
+        
         visited[src] = true;
+        
         while(!q.empty()){
             int front = q.front();
             q.pop();
@@ -24,20 +29,24 @@ class Solution {
                 if(!visited[nbr]){
                     q.push(nbr);
                     visited[nbr] = true;
-                    path[nbr] = front;
+                    parent[nbr] = front;
                 }
             }
         }
-        if (!visited[dest])
+        
+        if(!visited[dest]){
             return -1;
-        int count = 0;
-        int node = dest;
-        while(node != -1){
-            count++;
-            node = path[node];
         }
         
+        int node = dest;
+        int count = 0;
+        while(node != -1){
+            count++;
+            node = parent[node];
+        }
         
         return count-1;
+        
+        
     }
 };
