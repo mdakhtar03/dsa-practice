c90a06fb19ea526f03081ef22fda3e2bff0d2bcf
Md Akhtar <104804456+mdakhtar03@users.noreply.github.com>
2026-07-31 03:11:00 +0530

    Time: 11 ms (1.38%), Space: 8.3 MB (54.24%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> mp(8,0);
+        int j=0;int count=0;
+        for(int i=0;i<word.size();i++){
+
+            if(j==8){
+                j=0;
+            }
+            
+                mp[j]++;
+                count += mp[j];
+                j++;
+            
+             
+             
+        }
+        for( int &c:mp)
+        cout<<c<<" ";
+        
+        return count;
+    }
+};
