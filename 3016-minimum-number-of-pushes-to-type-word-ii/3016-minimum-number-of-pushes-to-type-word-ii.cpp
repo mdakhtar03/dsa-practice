9271d376a4ed5a5152ff7ff0f36b70632eb58741
Md Akhtar <104804456+mdakhtar03@users.noreply.github.com>
2026-07-31 21:40:57 +0530

    Time: 8 ms (61.48%), Space: 25.3 MB (84.45%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 
     int minimumPushes(string word) {
-        unordered_map<int,vector<int>> num_map; //2 3 4 5 6 7 8 9 
+        
         vector<int> char_map(26,0);
 
         for(char &ch:word){
@@ -17,23 +17,9 @@ public:
                     });
         
         int count = 0;
-        int j=2, i = 0;
-        while(i<char_map.size()){
-            if(j==10){
-                j=2;
-            }
-
-            num_map[j].push_back(char_map[i]);
-            
-            j++;
-            i++;
-
-        }
-        for(auto it:num_map){
-            for(int i=0;i<it.second.size();i++){
-                count += (i+1)*it.second[i];
-            }
-        }
+        for(int i=0;i<26;i++){
+            count += ((i/8)+1)*char_map[i];
+        }        
     return count;
     }
 };
